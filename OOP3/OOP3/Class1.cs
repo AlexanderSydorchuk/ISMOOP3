e63ca43fe5c3a98f788a843fecf56a94f062a2e7
AlexanderSydorchuk <72271589+AlexanderSydorchuk@users.noreply.github.com>
2020-12-12 17:42:04 +0200

    v.1

---
+++ OOP3/OOP3/Class1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace OOP3
+{
+    class Fraction
+    {
+        protected long Numerator;
+        protected long Denominator;
+
+        public Fraction(long Numerator = 1, long Denominator = 1)
+        {
+            this.Numerator = Numerator;
+            this.Denominator = Denominator;
+        }
+
+        public Fraction(string fraction)
+        {
+            Regex regex = new Regex(@"(\d+)\/(\d+)");
+            Match match = regex.Match(fraction);
+
+            Numerator = long.Parse(match.Groups[1].Value.ToString());
+            Denominator = long.Parse(match.Groups[2].Value.ToString());
+        }
+
+        public override string ToString()
+        {
+            return ($"{Numerator}/{Denominator}");
+        }
+
+        public static Fraction operator +(Fraction num1)
+        {
+            return num1;
+        }
+
+        public static Fraction operator-(Fraction num1)
+        {
+            num1.Numerator = -num1.Numerator;
+            return num1;
+        }
+
+        public static Fraction operator +(Fraction num1, Fraction num2)
+        {
+            Fraction result = new Fraction(1, 1);
+            if (num1.Denominator == num2.Denominator)
+            {
+                result.Denominator = num1.Denominator;
+                result.Numerator = num1.Numerator + num2.Numerator;
+            }
+            else if(num1.Denominator % num2.Denominator == 0 || num2.Denominator % num1.Denominator == 0)
+            {
+                if (num1.Denominator > num2.Denominator)
+                {
+                    result.Denominator = num1.Denominator;
+                    result.Numerator = num1.Numerator + num2.Numerator * (result.Denominator / num2.Denominator);
+                }
+                else
+                {
+                    result.Denominator = num2.Denominator;
+                    result.Numerator = num1.Numerator * (result.Denominator / num1.Denominator) + num2.Numerator;
+                }
+            }
+            else
+            {
+                result.Denominator = num1.Denominator * num2.Denominator;
+                result.Numerator = num1.Numerator * (result.Denominator / num1.Denominator) + num2.Numerator * (result.Denominator / num2.Denominator);
+            }
+
+            return result;
+        }
+
+        public static Fraction operator -(Fraction num1, Fraction num2)
+        {
+            Fraction result = new Fraction(1, 1);
+            if (num1.Denominator == num2.Denominator)
+            {
+                result.Denominator = num1.Denominator;
+                result.Numerator = num1.Numerator - num2.Numerator;
+            }
+            else if (num1.Denominator % num2.Denominator == 0 || num2.Denominator % num1.Denominator == 0)
+            {
+                if (num1.Denominator > num2.Denominator)
+                {
+                    result.Denominator = num1.Denominator;
+                    result.Numerator = num1.Numerator - num2.Numerator * (result.Denominator / num2.Denominator);
+                }
+                else
+                {
+                    result.Denominator = num2.Denominator;
+                    result.Numerator = num1.Numerator * (result.Denominator / num1.Denominator) - num2.Numerator;
+                }
+            }
+            else
+            {
+                result.Denominator = num1.Denominator * num2.Denominator;
+                result.Numerator = num1.Numerator * (result.Denominator / num1.Denominator) - num2.Numerator * (result.Denominator / num2.Denominator);
+            }
+
+            return result;
+        }
+
+        public static Fraction operator *(Fraction num1, Fraction num2)
+        {
+            Fraction result = new Fraction(1, 1);
+            result.Numerator = num1.Numerator * num2.Numerator;
+            result.Denominator = num1.Denominator * num2.Denominator;
+
+            return result;
+        }
+
+        public static Fraction operator /(Fraction num1, Fraction num2)
+        {
+            Fraction result = new Fraction(1, 1);
+            result.Numerator = num1.Numerator * num2.Denominator;
+            result.Denominator = num1.Denominator * num2.Numerator;
+
+            return result;
+        }
+
+
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        public static bool operator >(Fraction num1, Fraction num2)
+        {
+            return num1.Numerator * num2.Denominator > num2.Numerator * num1.Denominator;
+        }
+
+        public static bool operator <(Fraction num1, Fraction num2)
+        {
+            return num1.Numerator * num2.Denominator < num2.Numerator * num1.Denominator;
+        }
+
+        public static bool operator >=(Fraction num1, Fraction num2)
+        {
+            return num1.Numerator * num2.Denominator >= num2.Numerator * num1.Denominator;
+        }
+
+        public static bool operator <=(Fraction num1, Fraction num2)
+        {
+            return num1.Numerator * num2.Denominator <= num2.Numerator * num1.Denominator;
+        }
+
+        public static bool operator ==(Fraction num1, Fraction num2)
+        {
+            return num1.Numerator * num2.Denominator == num2.Numerator * num1.Denominator;
+        }
+
+        public static bool operator !=(Fraction num1, Fraction num2)
+        {
+            return num1.Numerator * num2.Denominator != num2.Numerator * num1.Denominator;
+        }
+
+        public static explicit operator double(Fraction num)
+        {
+            return num.Numerator * 1.0d / num.Denominator * 1.0d;
+        }
+
+        public void Normalize()
+        {
+            int i = 2;
+            while (Numerator >= i || Denominator >= i)
+            {
+                if(Numerator % i == 0 && Denominator % i == 0)
+                {
+                    Numerator /= i;
+                    Denominator /= i;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+
+
+    }
+}
---
+++ OOP3/OOP3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction drib = new Fraction(1,3);
+            Fraction drib2 = new Fraction(7);
+            Fraction drib3 = new Fraction("5/7");
+
+            Console.WriteLine($"{drib} > {drib2}: {drib > drib2}");
+            Console.WriteLine($"{drib} < {drib2}: {drib < drib2}");
+            Console.WriteLine($"{drib} = {drib3}: {drib == drib3}");
+            Console.WriteLine($"{drib} != {drib3}: {drib != drib3}");
+
+            Console.WriteLine();
+
+            Fraction drib4 = new Fraction();
+            Fraction drib5 = new Fraction();
+            Fraction drib7 = new Fraction();
+            Fraction drib8 = new Fraction();
+            Fraction drib9 = new Fraction();
+            Fraction drib10 = new Fraction();
+
+            drib4 = drib2 + drib3;
+            drib5 = drib2 - drib3;
+            drib7 = drib + drib3;
+            drib8 = drib * drib3;
+            drib9 = drib / drib3;
+            drib10 = -drib3;
+
+            Console.WriteLine($"{drib2} + {drib3} = {drib4}");
+            Console.WriteLine($"{drib2} - {drib3} = {drib5}");
+            Console.WriteLine($"{drib} + {drib3} = {drib7}");
+            Console.WriteLine($"{drib} * {drib3} = {drib8}");
+            Console.WriteLine($"{drib} / {drib3} = {drib9}");
+            Console.WriteLine($"-drib3 = {drib10}");
+
+            Console.WriteLine();
+
+            Fraction drib6 = new Fraction("26/78");
+            Console.WriteLine($"Скоротити дрiб: {drib6}");
+            drib6.Normalize();
+            Console.WriteLine($"{drib6}");
+
+        }
+    }
+}
